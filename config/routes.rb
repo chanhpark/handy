4a0e77778ed3f696d71fd3a7fe7db35d2982175e
chanhpark <chanhyun.park1211@gmail.com>
2015-06-04 11:56:33 -0400

    functionality for add courses

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :forms
+
   root 'homes#index'
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
